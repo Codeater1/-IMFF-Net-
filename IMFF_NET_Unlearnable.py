893e25df8bfa19b9574ea43170717ab871d1b431
Oliver Zhang <127297716+Codeater1@users.noreply.github.com>
2025-06-04 14:13:56 +0800

    Add files via upload

---
+++ IMFF_NET_Unlearnable.py
@@ -0,0 +1,5 @@
+ucm: http://weegee.vision.ucmerced.edu/datasets /landuse.html
+AID: www.lmars.whu.edu.cn/xia/AlD-project.html
+WHU-RS19: http://gpev.whu.edu.cn/data
+NWPU-RESISC45: https://gcheng-nwpu.github.io 
+RSSCN7: https://sites.google.com/site/qinzoucn/documents
