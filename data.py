ec520ebdeb178baea2e3c529b743790975d7ecdd
Oliver Zhang <127297716+Codeater1@users.noreply.github.com>
2025-06-08 17:36:53 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
-ucm: http://weegee.vision.ucmerced.edu/datasets /landuse.html
-AID: www.lmars.whu.edu.cn/xia/AlD-project.html
-WHU-RS19: http://gpev.whu.edu.cn/data
+The UCMerced: http://weegee.vision.ucmerced.edu/datasets/landuse.html
+WHU-RS19: https://captain-whu.github.io/BED4RS/#
+RSSCN7:https://drive.google.com/drive/folders/1A05g8Y0Nj2YZ7XdoJMA9p3rVsN40svCx
+The AID: https://pan.baidu.com/s/1mifOBv6#list/path=%2F
 NWPU-RESISC45: https://gcheng-nwpu.github.io 
-RSSCN7: https://sites.google.com/site/qinzoucn/documents
